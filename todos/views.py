d5152950e7660c87e4f846794393d385fd5830ed
AgathaBahati <matinodev@gmail.com>
2024-06-27 15:03:23 +0300

    feat: add search view
    - Add search view and url
    
    - Modify index.html to implement htmx atrributes for search
    
    - Add todo.html template to list todo items

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from .todo import todos
 
 def index(request):
     return render(request, 'index.html')
+
+@require_http_methods(['POST'])
+def search(request):
+    found_todos = []
+    search_term = request.POST.get('search')
+    if len(search_term) == 0:
+        return render(request, 'todo.html', {'todos': []})
+    for todo in todos:
+        if search_term in todo['title']:
+            found_todos.append(todo)
+    return render(request, 'todo.html', {'todos': found_todos})
